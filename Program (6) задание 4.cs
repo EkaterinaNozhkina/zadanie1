624204a60cbd2db463a3739430c966f2e5f0310f
Екатерина Ножкина <65303200+EkaterinaNozhkina@users.noreply.github.com>
2020-05-19 13:13:16 +0500

    Add files via upload

---
+++ Program (6) задание 4.cs
@@ -0,0 +1,109 @@
+﻿using System;
+
+using static System.Convert;
+
+using static System.Console;
+
+using System.Collections;
+
+namespace ConsoleApp29
+
+{
+
+    class Program
+
+    {
+
+        static void Main(string[] args)
+
+        {
+
+            ArrayList list = new ArrayList();
+
+            list.Add('N');
+
+            list.Add(-9);
+
+            list.Add('M');
+
+            list.Add(95);
+
+            list.Add(-47);
+
+            list.Add('P');
+
+            list.Add(-1);
+
+            list.Add(19);
+
+            list.Add('e');
+
+            list.Add(7);
+
+            list.Add(-4);
+
+            list.Add('O');
+
+            list.Add(-13);
+
+            list.Add('K');
+
+            ArrayList list2 = new ArrayList();
+
+            WriteLine("Числа: ");
+
+            for (int j = 0; j < list.Count; j++)
+
+            {
+
+                int y = 0;
+
+                string i = list[i] + "";
+
+                bool rez = int.TryParse(i, out y); ;
+
+                if (rez == false)
+
+                {
+
+                    if (i == i.ToUpper())
+
+                    {
+
+                        list2.Add(list[j]);
+
+                        list.RemoveAt(j);
+
+                    }
+
+                    else list.RemoveAt(j);
+
+                }
+
+            }
+
+            foreach (object o in list)
+
+            {
+
+                WriteLine(o);
+
+            }
+
+            WriteLine("Буквы заглавные: ");
+
+            foreach (object o in list2)
+
+            {
+
+                WriteLine(o);
+
+            }
+
+            ReadLine();
+
+        }
+
+    }
+
+}
---
+++ Program_1 (1) задание 3.cs
@@ -0,0 +1,135 @@
+﻿using System;
+
+using static System.Console;
+
+using static System.Convert;
+
+namespace ConsoleApp28
+
+{
+
+    class Program
+
+    {
+
+        static void Main(string[] args)
+
+        {
+
+            WriteLine("2 задание; \n1 задание; \n3 задание; \n");
+
+            int h = ToInt32(ReadLine());
+
+            if h == 1)
+
+                if (h == 2)
+
+                {
+
+                    int[] mas = new int[10] { 8, 16, 13, 22, 87, 2, 6, 6, 8, -2 };
+
+                    string rez = "";
+
+                    int count = 0;
+
+                    for (int j = 0; j < mas.Length; j++)
+
+                    {
+
+                        for (int j = 0; j < mas.Length; j++)
+
+                        {
+
+                            if (mas[j] == mas[j])
+
+                            {
+
+                                count++;
+
+                            }
+
+                        }
+
+                        if (count == 1) rez = rez + mas[j] + "; ";
+
+                        count = 0;
+
+                    }
+
+                    WriteLine("особенные элементы массива: " + rez);
+
+                }
+
+                else if (h == 1)
+
+                {
+
+                    int[] mas = new int[5] { 9, -3, -1, 2, -6 };
+
+                    int min = mas[0];
+
+                    int minnom = 0;
+
+                    for (int j = 0; j < mas.Length; j++)
+
+                    {
+
+                        if (Math.Abs(mas[j]) < min)
+
+                        {
+
+                            min = Math.Abs(mas[j]);
+
+                            minnom = j;
+
+                        }
+
+                    }
+
+                    WriteLine("Минимальное число: {min} и его индекс: {minnom}");
+
+else if (h == 3)
+
+                    {
+
+                        int[] mas = new int[10];
+
+                        Random rand = new Random();
+
+                        WriteLine();
+
+                        for (int j = 0; j < 10; j++)
+
+                        {
+
+                            mas[j] = rand.Next(-10, 10);
+
+                            Write(mas[j] + " ");
+
+                        }
+
+                        Write("\nПоложительные элементы массива: ");
+
+                        for (int j = 0; j < 10; j++)
+
+                        {
+
+                            if (mas[j] < 0)
+
+                            {
+
+                                mas[j] = 0;
+
+                            }
+
+                            else Write(mas[j] + " ");
+
+                        }
+
+                    }
+
+                }
+
+        }
+
+    }
